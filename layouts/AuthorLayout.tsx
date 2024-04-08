2a3b7ed801387cb07debfa7728d9a8c0dbb5bd35
李文瑞 <raven@xverse.cn>
2024-04-08 14:59:29 +0800

    fix

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github, bilibili } = content
+  const { name, avatar, occupation, company, email, twitter, linkedin, github } = content
 
   return (
     <>
